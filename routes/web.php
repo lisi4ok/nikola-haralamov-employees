ef8578ea691392da2d254a601e170c3755a73629
Zaio Klepoyshkov <master@lisi4ok.com>
2024-11-21 01:36:45 -0800

    Small comment fix

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', [PageController::class, 'index']);
 Route::post('/', [PageController::class, 'upload']);
